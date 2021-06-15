d59773a0f83aa36e0f8d4257ffdb32699c268cf5
Vickbart <83325452+Dramaturgo@users.noreply.github.com>
2021-06-15 08:30:14 -0500

    Update ImprimirArregloDoubleFormato.java

--- ImprimirArregloDoubleFormato.java
+++ ImprimirArregloDoubleFormato.java
@@ -1,38 +1,18 @@
 public class ImprimirArregloDoubleFormato{
 
-public static String Imprime_arreglo(double []arreglo){
+public static String imprimeArregloDouble(double []arreglo){
 String cadena="";
 for(int i=0;i<arreglo.length;i++){
-    cadena+=String.format("%.4s",String.valueOf(arreglo[i]))+" ";
+    cadena+=String.format("%.2f",arreglo[i])+"% ";
 
 }return cadena;
 }
 
-
-public static String Imprime_arregloV2(double[]arreglo){
-String cadenas="";
-
-for(int i =0;i<arreglo.length;i++){
-cadenas+=String.format("%.2f",arreglo[i])+" ";
-
-}return cadenas;
-
-}
-
-
-
 public static void main(String[] args) {
 	
 double[]arreglo={0.12456,0.13566,0.154566};
-System.out.println(arreglo[0]);
 
-String mona="";
-mona=String.format("%.4s",arreglo[0]);
-System.out.println(mona);
-
-//System.out.println(Imprime_arreglo(arreglo));
-
-System.out.println(Imprime_arregloV2(arreglo));
+System.out.println("["+imprimeArregloDouble(arreglo)+"]");
 
 
 
